73d3455afb9e920d66830802a7f2a7a01850d972
unknown <ola.ses@yahoo.com>
2022-02-18 05:53:41 +0100

    modify and category

--- template1/index.php
+++ template1/index.php
@@ -10,7 +10,8 @@ if(isset($_SESSION['user_id'])){
 <a href="promotional-gif.php"><button type="button" class="btn btn-danger btn-lg">Promotional GIF banners</button></a>
 <a href="slider-banners.php"><button type="button" class="btn btn-primary btn-lg">Slider banner</button></a>
 <a href="banners.php"><button type="button" class="btn btn-secondary btn-lg">Banners</button></a>
-<a href="product-categories.php"><button type="button" class="btn btn-info btn-lg">Product categories</button></a>
+<a href="products-categories.php"><button type="button" class="btn btn-info btn-lg">Products categories</button></a>
+<a href="products-subcategories.php"><button type="button" class="btn btn-dark btn-lg">Products subcategories</button></a>
 <a href="products.php"><button type="button" class="btn btn-warning btn-lg">Products</button></a>
 </div>
 </div>';
